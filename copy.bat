e89b5cc9f0a61f11e60d884572ba58ee9a1538b8
Alexander Semeliker <alexander.semeliker@siemens.com>
2017-10-06 15:45:01 +0200

    added msg to dev folder

--- copy.bat
+++ copy.bat
@@ -1,3 +1,5 @@
+@echo off
+
 xcopy D:\_Diplomarbeit\HOBBIT\blockly\blockly_accessible_compressed.js D:\_Diplomarbeit\HOBBIT\dev /Y
 xcopy D:\_Diplomarbeit\HOBBIT\blockly\blockly_accessible_uncompressed.js D:\_Diplomarbeit\HOBBIT\dev /Y
 xcopy D:\_Diplomarbeit\HOBBIT\blockly\blockly_compressed.js D:\_Diplomarbeit\HOBBIT\dev /Y
@@ -6,4 +8,5 @@ xcopy D:\_Diplomarbeit\HOBBIT\blockly\blocks_compressed.js D:\_Diplomarbeit\HOBB
 xcopy D:\_Diplomarbeit\HOBBIT\blockly\python_compressed.js D:\_Diplomarbeit\HOBBIT\dev /Y
 xcopy D:\_Diplomarbeit\HOBBIT\blockly\blocks\hobbit.js D:\_Diplomarbeit\HOBBIT\dev\blocks\hobbit.js /Y
 xcopy D:\_Diplomarbeit\HOBBIT\blockly\core D:\_Diplomarbeit\HOBBIT\dev\core /Y
+xcopy D:\_Diplomarbeit\HOBBIT\blockly\msg D:\_Diplomarbeit\HOBBIT\dev\msg /Y
 xcopy D:\_Diplomarbeit\HOBBIT\blockly\generators D:\_Diplomarbeit\HOBBIT\dev\generators /Y
